fbc42d972bca74db024dea124d4506b86fed4560
Changhua <lichanghua0821@gmail.com>
2023-02-24 08:40:58 +0800

    Clean up

--- nodejs/proto/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.protoIndex = void 0;
-require("./wcf_grpc_pb");
-require("./wcf_pb");
-const protoIndex = () => {
-};
-exports.protoIndex = protoIndex;
--- nodejs/proto/index.ts
+++
@@ -1,5 +0,0 @@
-import './wcf_grpc_pb';
-import './wcf_pb';
-
-export const protoIndex:any = ():void => {
-};
