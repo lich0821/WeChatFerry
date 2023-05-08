beb5e482228dc5116532c1995473bfbf976b352b
Changhua <lichanghua0821@gmail.com>
2023-05-08 16:58:05 +0800

    Fix wcferry version

--- http/setup.py
+++ http/setup.py
@@ -31,7 +31,7 @@ setup(
     },
     install_requires=[
         "setuptools",
-        f"wcferry=={wcfhttp.core.__version__}",
+        f"wcferry<={wcfhttp.core.__version__}",
         "fastapi",
         "uvicorn[standard]"
     ],
