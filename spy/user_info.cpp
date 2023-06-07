e70fa39ad8f52aa0abacc7724cb0ab89b9b5f59b
Changhua <lichanghua0821@gmail.com>
2023-06-07 15:50:12 +0800

    Fix Functions_FUNC_GET_USER_INFO

--- spy/user_info.cpp
+++ spy/user_info.cpp
@@ -6,14 +6,23 @@
 extern WxCalls_t g_WxCalls;
 extern DWORD g_WeChatWinDllAddr;
 
-string GetHomePath() { return GET_STRING(g_WeChatWinDllAddr + g_WxCalls.ui.home); }
+static char home[MAX_PATH] = { 0 };
+
+string GetHomePath()
+{
+    if (home[0] == 0) {
+        string path = Wstring2String(GET_WSTRING(g_WeChatWinDllAddr + g_WxCalls.ui.home));
+        strncpy_s(home, path.c_str(), path.size());
+    }
+
+    return string(home);
+}
 
 string GetSelfWxid()
 {
     DWORD wxidType = 0;
     try {
         wxidType = GET_DWORD(g_WeChatWinDllAddr + g_WxCalls.ui.wxid + 0x14);
-        LOG_DEBUG("WeChatWinDll: {:#x}, wxid type: {:#x}", g_WeChatWinDllAddr, wxidType);
         if (wxidType == 0xF) {
             return GET_STRING_FROM_P(g_WeChatWinDllAddr + g_WxCalls.ui.wxid);
         } else {
@@ -33,7 +42,7 @@ UserInfo_t GetUserInfo()
     ui.wxid   = GetSelfWxid();
     ui.name   = GET_STRING_FROM_P(g_WeChatWinDllAddr + g_WxCalls.ui.nickName);
     ui.mobile = GET_STRING_FROM_P(g_WeChatWinDllAddr + g_WxCalls.ui.mobile);
-    ui.home   = Wstring2String(GET_WSTRING(g_WeChatWinDllAddr + g_WxCalls.ui.home));
+    ui.home   = GetHomePath();
 
     return ui;
 }
