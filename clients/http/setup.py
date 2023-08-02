68a8e7292638721580d5de52a0d5b55c121537fa
Changhua <lichanghua0821@gmail.com>
2023-08-03 00:50:35 +0800

    v39.0.2.1

--- clients/http/setup.py
+++ clients/http/setup.py
@@ -33,7 +33,7 @@ setup(
         "setuptools",
         "fastapi",
         "uvicorn[standard]",
-        "wcferry>=39.0.2.0",
+        "wcferry>=39.0.2.1",
     ],
     classifiers=[
         "Environment :: Win32 (MS Windows)",
