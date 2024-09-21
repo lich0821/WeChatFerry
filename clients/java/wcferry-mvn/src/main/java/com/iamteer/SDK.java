b56ccfb421fbea815089a5edbec75ee6dcbb1024
chandler <1915724901@qq.com>
2024-09-21 20:22:28 +0800

    feat(0): Java版增加Maven版(基于原本的gradle改造)

---
+++ clients/java/wcferry-mvn/src/main/java/com/iamteer/SDK.java
@@ -0,0 +1,30 @@
+package com.iamteer;
+
+import com.sun.jna.Library;
+
+/**
+ * SDK.dll的接口类
+ * 
+ * @Description
+ * @Author xinggq
+ * @Date 2024/7/10
+ */
+public interface SDK extends Library {
+
+    /**
+     * 初始化SDK
+     * 
+     * @param debug
+     * @param port
+     * @return
+     */
+    int WxInitSDK(boolean debug, int port);
+
+    /**
+     * 退出SDK
+     * 
+     * @return
+     */
+    int WxDestroySDK();
+
+}
---
+++ clients/java/wcferry-mvn/src/main/java/com/iamteer/WcferryApplication.java
@@ -0,0 +1,19 @@
+package com.iamteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * 
+ * @author chandler
+ * @date 2024-09-21 12:19
+ */
+@SpringBootApplication
+public class WcferryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WcferryApplication.class, args);
+    }
+
+}
