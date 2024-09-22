6863a7f99c91e3fcf4967869f8779b2c00b28717
chandler <1915724901@qq.com>
2024-09-22 19:22:13 +0800

    feat(0): [java]-[wcferry-mvn]-1.maven插件中集成protobuf插件用于生成proto文 2.定义了proto配置序列化

---
+++ clients/java/wcferry-mvn/src/main/java/com/iamteer/config/ProtobufConfig.java
@@ -0,0 +1,35 @@
+package com.iamteer.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 配置类-protobuf
+ *
+ * @author chandler
+ * @date 2024-09-26 21:35
+ */
+@Configuration
+public class ProtobufConfig {
+
+    /**
+     * protobuf 序列化
+     */
+    @Bean
+    ProtobufHttpMessageConverter protobufHttpMessageConverter() {
+        return new ProtobufHttpMessageConverter();
+    }
+
+    /**
+     * protobuf 反序列化
+     */
+    @Bean
+    RestTemplate restTemplate(ProtobufHttpMessageConverter protobufHttpMessageConverter) {
+        return new RestTemplate(Collections.singletonList(protobufHttpMessageConverter));
+    }
+
+}
