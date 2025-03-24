59bccbb93112d2684a3bfbb69061228e91d2d76c
Changhua <lichanghua0821@gmail.com>
2025-03-24 22:50:15 +0800

    chore(python): bump to v39.4.4.0

--- clients/python/wcferry/client.py
+++ clients/python/wcferry/client.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__version__ = "39.4.3.0"
+__version__ = "39.4.4.0"
 
 import atexit
 import base64
@@ -409,7 +409,6 @@ class Wcf():
         Returns:
             int: 0 为成功，其他失败
         """
-        raise Exception("Not implemented, yet")
         req = wcf_pb2.Request()
         req.func = wcf_pb2.FUNC_SEND_XML  # FUNC_SEND_XML
         req.xml.receiver = receiver
